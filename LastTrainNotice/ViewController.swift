55842b34533dc0e52dd6ea4eebaf9755626868bc
Kipeo <kp225048@gmail.com>
2024-09-16 12:07:56 +0900

    Initial Commit

---
+++ LastTrainNotice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LastTrainNotice
+//
+//  Created by x22086xx on 2024/09/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
